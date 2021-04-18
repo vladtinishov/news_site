4eef0f93c3c6501dc0192085659728ca1c163ed7
Vladislav <vladislav04022001@gmail.com>
2021-04-18 17:12:06 +0600

    Создал проект, добавил разметку header и footer и файл стилей assets/css/header.css

---
+++ application/views/templates/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>News Site</title>
+</head>
+<body>
+    <style><?php require_once('assets/css/header.css');?></style>
+	<nav id="header">
+		<ul id="header_outer_ul">
+			<div id="header_logo">LOGO</div>
+			<ul id="header_links">
+				<li><a href="#">Главная</a></li>
+				<li><a href="#">Новости на неделю</a></li>
+				<li><a href="#">Архив</a></li>
+			</ul>
+			<ul>
+				<li><a href="#">t</a></li>
+				<li><a href="#">vk</a></li>
+			</ul>
+		</ul>
+	</nav>
