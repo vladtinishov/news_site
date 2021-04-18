69963ede7654f6b27d47b08dc6a7d27b9dbe4361
Vladislav <vladislav04022001@gmail.com>
2021-04-18 18:40:03 +0600

    сделал разметку для вывод всех новостей в all_news.php

--- application/views/all_news.php
+++ application/views/all_news.php
@@ -2,6 +2,21 @@
 
 <div class="news_list_style">
 
+    <?php foreach($news_data as $news): ?>
+        
+        <div class="news_header">
+            <h1 class="news_title"><?php echo $news->news_title?></h1>
+            <div class="news_date"><?php echo $news->news_date?></div>
+        </div>
+
+        <p class="news_text"><?php echo $news->news_text ?></p>
+
+        <?php if($news != ''):?>
+            <img src="<?php $news->news_img?>" alt="">
+        <?php endif?>
+
+    <?php endforeach?>
+
 </div>
 
     
